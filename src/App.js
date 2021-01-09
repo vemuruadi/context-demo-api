76462b0fd90ab0c7a2dfc4c00873e68a619c2166
Adi Narayana Vemuru <vemuruadi@gmail.com>
2021-01-09 14:15:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Library from './components/library';
+import LibraryState from './context/libraryState';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container-fluid pt-4 pl-4">
+        <h2>
+          React Context API Demo{" "}
+        </h2>
+        <LibraryState>
+          <Library />
+        </LibraryState>
+      </div>
+    </React.Fragment>
   );
 }
 
